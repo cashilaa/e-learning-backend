394a8b222a617faf067051aaf18706ef03314301
sheila wafula <167983359+cashilaa@users.noreply.github.com>
2024-11-06 18:58:47 +0300

    Delete dist directory

--- dist/auth/auth.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { AuthService } from './auth.service';
-import { Response } from 'express';
-export declare class AuthController {
-    private readonly authService;
-    constructor(authService: AuthService);
-    signin(req: any): Promise<void>;
-    googleAuth(req: any): Promise<void>;
-    googleAuthRedirect(req: any, res: Response): Promise<void>;
-}
