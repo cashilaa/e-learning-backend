0e63ae93b6102a246f98646d9d7af4e7ef95b9f6
sheila wafula <167983359+cashilaa@users.noreply.github.com>
2024-11-06 13:26:01 +0300

    Delete src directory

--- src/auth/user.entity.ts
+++
@@ -1,8 +0,0 @@
-// src/auth/user.entity.ts
-export class User {
-    id: number;
-    email: string;
-    password?: string;  // Make password optional for Google login
-    userType: string;
-  }
-  
